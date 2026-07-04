3a3d3e4b7128ce57c6b8eb7a71fccad2a9e1d5d1
medhamallick <hdb.medha@gmail.com>
2026-07-04 13:11:38 +0530

    "Fix API base URL for production"

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,1 @@
-// export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/api";
-
-
-
-
-export const BASE_URL = "https://tic-tac-toe-server-0bf6.onrender.com";
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/api";
